476da3d81d0385662a319454bb7fbff4cd786e40
caleb byrne <calebmbyrne@gmail.com>
2025-10-02 11:45:48 -0700

    removed the as EC from the import statement

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 
 
 def wait_for_element(driver, locator, timeout=10):
     """Explicit wait for an element to be visible."""
-    return WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator))
+    return WebDriverWait(driver, timeout).until(expected_conditions.visibility_of_element_located(locator))
